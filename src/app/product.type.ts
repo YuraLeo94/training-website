ef3cde055ac6c0061b82c0a6daaf8ed7aefcc554
Leonovics <jurijs.leonovics@accenture.com>
2021-12-01 16:08:50 +0200

    commit1

--- src/app/product.type.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id: number;
-    title: string;
-    description: string;
-    image: string;
-    cost: number;
-    imageAlt: string;
-    rating: number;
-}
--- src/app/shared/global.const.ts
+++
@@ -1,2 +0,0 @@
-export const MIN_AMOUNT = 1;
-export const MAX_AMOUNT = 1000;
